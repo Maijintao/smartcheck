01ef3641ef9a6f8057e359bbe4794be86f4f031d
wcx <2472704646@qq.com>
2026-09-10 13:35:13 +0800

    chore: 发布 1.0.32 版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.smartcheck.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 33
-        versionName = "1.0.31"
+        versionCode = 34
+        versionName = "1.0.32"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
